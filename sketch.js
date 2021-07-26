c8351a17bb2718cd730b01c766ce8adfa2044e09
Rohaniscool12 <87386511+Rohaniscool12@users.noreply.github.com>
2021-07-26 13:31:53 -0600

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,21 @@
+var weight = [40,35,45,40,40,28,58,68];
+
+function setup() {
+  createCanvas(400,400);
+
+  var sum = weight[0] + weight[1] + weight[2] + weight[3]+weight[4];
+  var average = sum/weight.length;
+  console.log(weight[3]);
+
+}
+
+
+  
+
+function draw() 
+{
+ 
+background(51);
+
+}
+
